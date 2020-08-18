eb8baf393a8c21dca9bc9e307bd0d526076f6fa6
zhangyu <13100650510@163.com>
2020-08-18 17:33:13 +0800

    fix:es接口

--- base-es-service/src/main/java/com/itzy/baseesservice/config/BaseElasticsearchConfig.java
+++ base-es-service/src/main/java/com/itzy/baseesservice/config/BaseElasticsearchConfig.java
@@ -3,6 +3,7 @@ package com.itzy.baseesservice.config;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -34,6 +35,16 @@ public class BaseElasticsearchConfig {
     @Value("${elasticsearch.port}")
     private int port;
 
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
     /**
      * LowLevelRestConfig
      *
@@ -47,7 +58,7 @@ public class BaseElasticsearchConfig {
         // 设置Header编码
         Header[] defaultHeaders = {new BasicHeader("content-type", "application/json")};
         clientBuilder.setDefaultHeaders(defaultHeaders);
-        // 添加其他配置，这些配置都是可选的，详情配置可看https://blog.csdn.net/jacksonary/article/details/82729556
+        // 添加其他配置，这些配置都是可选的
         return clientBuilder.build();
     }
 
--- base-es-service/src/test/java/com/itzy/baseesservice/SearchApplicationTests.java
+++ base-es-service/src/test/java/com/itzy/baseesservice/SearchApplicationTests.java
@@ -1,9 +1,18 @@
 package com.itzy.baseesservice;
 
+import com.alibaba.fastjson.JSON;
+import com.itzy.baseesservice.config.BaseElasticsearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 /**
  * @author ZY
@@ -12,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @Version 1.0
  */
 @SpringBootTest
+@EnableAutoConfiguration
 public class SearchApplicationTests {
     @Autowired
     private RestHighLevelClient client;
@@ -20,4 +30,41 @@ public class SearchApplicationTests {
     public void contextLoads() {
         System.out.println(client);
     }
+
+    /**
+     * 测试存储数据到es
+     * 更新也是可以的
+     */
+    @Test
+    public void indexData() throws IOException {
+        // 准备数据
+        IndexRequest indexRequest = new IndexRequest("users");//索引名
+        indexRequest.id("1");//数据id
+
+        // 第一种方式
+//        indexRequest.source("userName", "zhangsan", "age", 18, "gender", "男");
+
+        // 第二种方式（推荐使用）
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setAge(18);
+        user.setGender("男");
+
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON); //要保存的内容
+
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, BaseElasticsearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的数据相应
+        System.out.println(index);
+    }
+
+    @Data
+    class User{
+        private String userName;
+        private Integer age;
+        private String gender;
+    }
+
 }
